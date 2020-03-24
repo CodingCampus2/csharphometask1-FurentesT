f691c84e283ae91e53d08bf3571205d2f3de7379
Taras Pobyvanets <furentes.t@gmail.com>
2020-03-24 21:19:39 +0200

    Implemented homework

--- Program.cs
+++ Program.cs
@@ -12,8 +12,15 @@ namespace HomeworkTemplate
                 // Your solution goes here
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
+                float numberOfSickPeople = float.Parse(task.City.Population) * float.Parse(task.City.SickPercentage);
+                float numberOfDeath = float.Parse(task.Virus.KillProbability) * numberOfSickPeople;
 
-                return "My answer";
+                string answer = "There are " + Math.Truncate(numberOfSickPeople)
+                + " people sick with " + task.Virus.Name
+                + " in the city of " + task.City.Name + ", "
+                + Math.Truncate(numberOfDeath) + " of which died";
+
+                return answer;
             };
 
             Task1.CheckSolver(TaskSolver);
